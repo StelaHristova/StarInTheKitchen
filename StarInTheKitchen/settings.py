2a40a2b0be91e259cca8a984f85ed514f26c4562
Stela Hristova <stela.m.hristova@gmail.com>
2025-08-07 00:27:09 +0300

    fix:settings.py

--- StarInTheKitchen/settings.py
+++ StarInTheKitchen/settings.py
@@ -29,15 +29,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
-DEBUG = True if config('DEBUG') == "True" else False
+DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
+# DEBUG = True if config('DEBUG') == "True" else False
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
-
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS').split(' ')
+# CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
 
 # CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=lambda v: [s.strip() for s in v.split(',')])
 
