76c18f5cbc4730e454aa3b561689337ea8a72e3d
Stela Hristova <stela.m.hristova@gmail.com>
2025-08-08 02:56:47 +0300

    fix:update startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 cd /home/site/wwwroot
 
+if [ -d "../.venv" ]; then
+  source ../.venv/bin/activate
+fi
+
 python manage.py collectstatic --noinput
 
 exec gunicorn StarInTheKitchen.wsgi:application --bind=0.0.0.0
