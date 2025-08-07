14b6f6cf7eae2c95ca9c4b3ce92885f83ca9ad37
Stela Hristova <stela.m.hristova@gmail.com>
2025-08-08 02:43:49 +0300

    fix:update startup

--- startup.sh
+++ startup.sh
@@ -1,15 +1,5 @@
 cd /home/site/wwwroot
 
-if [ -d .venv ]; then
-  source .venv/bin/activate
-fi
-
-if [ -f .env ]; then
-  export $(cat .env | xargs)
-fi
-
-#celery -A StarInTheKitchen worker --loglevel=info --detach
-
 python manage.py collectstatic --noinput
 
 exec gunicorn StarInTheKitchen.wsgi:application --bind=0.0.0.0
