2f35691e8516eb071b0350d673820b58a9a1ff7b
Stela Hristova <stela.m.hristova@gmail.com>
2025-08-07 18:39:09 +0300

    fix:startup

--- startup.sh
+++ startup.sh
@@ -1,8 +1,1 @@
-cd /home/site/wwwroot/StarInTheKitchen
-
-if [ -d "../antenv" ]; then
-    source ../antenv/bin/activate
-fi
-
-
-exec gunicorn StarInTheKitchen.wsgi:application --bind=0.0.0.0
+gunicorn StarInTheKitchen.wsgi:application --bind=0.0.0.0
