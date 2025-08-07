de93a81fcb5cfabc403921fa39de0367daeaf445
Stela Hristova <stela.m.hristova@gmail.com>
2025-08-08 02:14:16 +0300

    fix:update startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 cd /home/site/wwwroot
 
+celery -A StarInTheKitchen worker --loglevel=info &
+
 python manage.py collectstatic --noinput
 
 exec gunicorn StarInTheKitchen.wsgi:application --bind=0.0.0.0
